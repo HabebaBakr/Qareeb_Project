d221f95a24094a5e3b20404be135f881abf4727e
habiba <habebabakr4@.com>
2024-05-02 05:34:16 +0300

    connect to database

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -35,14 +35,14 @@ export default function Header() {
       <header className='bg-sky-500 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <h1 className='font-blod text-sm sm:text-xl flex flex-wrap'>
-           <span className='text-slate-500'>Q</span>
+           <span className='text-slate-500 '>Q</span>
            <span className='text-slate-700'>areeb</span>
         </h1>
         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
            <input
            type='text'
            placeholder='Search...' 
-           className='bg-transparent focus:outline-none w-24 sw:w-64'
+           className='bg-transparent focus:outline-none w-30 sw:w-64'
            />
           <FaSearch className='text-slate-600'/>
        </form>
